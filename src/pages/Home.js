2d9050217e8402755c4cc268aeec8a5fcf36ef16
Dmitrii Prokofev <dmitriiprokofev@MacBook-Pro-Dmitrii.local>
2019-05-14 23:20:58 -0400

    created routes and an attempt to create the main page layout, deleted unnecessary files

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      Something to display
+        </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/Workout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Workout() {
+  return (
+    <div>
+      Another thing to display
+        </div>
+  );
+}
+
+export default Workout;
