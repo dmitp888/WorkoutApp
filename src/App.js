ccdf0067d23b0143b9bc89b46182da38238146c7
Dmitrii Prokofev <dmitriiprokofev@MacBook-Pro-Dmitrii.local>
2019-05-15 12:05:14 -0400

    routes fixed but duplicating position

--- src/App.js
+++ src/App.js
@@ -1,28 +1,100 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+// import {  Link } from 'react-router-dom'
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Workout from "./pages/Workout";
 import Wrapper from "./components/Wrapper";
+import Signup from './components/sign-up'
+import LoginForm from './components/login-form'
+import axios from 'axios'
 
-function App() {
-  return (
-    <Router>
-    <div>
-      <Navbar />
-      <Wrapper>
-        <Route exact path="/" component={About} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/workout" component={Workout} />
-      </Wrapper> 
-       <Footer />
-    </div>
-  </Router>
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      loggedIn: false,
+      username: null
+    }
 
-  );
+    this.getUser = this.getUser.bind(this)
+    this.componentDidMount = this.componentDidMount.bind(this)
+    this.updateUser = this.updateUser.bind(this)
+  }
+
+  componentDidMount() {
+    this.getUser()
+  }
+
+  updateUser (userObject) {
+    this.setState(userObject)
+  }
+
+  getUser() {
+    axios.get('/user/').then(response => {
+      console.log('Get user response: ')
+      console.log(response.data)
+      if (response.data.user) {
+        console.log('Get User: There is a user saved in the server session: ')
+
+        this.setState({
+          loggedIn: true,
+          username: response.data.user.username
+        })
+      } else {
+        console.log('Get user: no user');
+        this.setState({
+          loggedIn: false,
+          username: null
+        })
+      }
+    })
+  }
+
+  render() {
+    return (
+      <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/workout" component={Workout} />
+        </Wrapper> 
+         <Footer />
+      </div>
+      <div className="App">
+        <Navbar updateUser={this.updateUser} loggedIn={this.state.loggedIn} />
+        {/* greet user if logged in: */}
+        {this.state.loggedIn &&
+          <p>Join the party, {this.state.username}!</p>
+        }
+        {/* Routes to different components */}
+        <Route
+          exact path="/"
+          component={Home} />
+        <Route
+          path="/login"
+          render={() =>
+            <LoginForm
+              updateUser={this.updateUser}
+            />}
+        />
+        <Route
+          path="/signup"
+          render={() =>
+            <Signup/>}
+        />
+      </div>
+      </Router>
+
+    );
+  }
 }
+
+
 
 export default App;
