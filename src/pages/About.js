5c8d2c68c40f143ac404227e607b91088299c8a3
Dmitrii Prokofev <dmitriiprokofev@MacBook-Pro-Dmitrii.local>
2019-05-15 10:44:50 -0400

    updated About page design

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,15 +4,14 @@ import "../style.css";
 function About() {
   return (
     <div  >
-      <div className="jumbotron jumbotron-fluid" >
+      {/* <div className="jumbotron jumbotron-fluid" > */}
         <div className="container">
           <h1 className="display-4">The workout routines that fit best in your life</h1>
           <p className="lead">Change your lifestyle today</p>
-          <img src="https://legionathletics.com/wp-content/uploads/2016/03/xmaximum-hypertrophy-workout.jpg.pagespeed.ic.tv63zEm8Rs.jpg" className="img-fluid" alt="Workout"></img>
         </div>
       </div>
 
-    </div>
+    //  </div>
   );
 };
 
