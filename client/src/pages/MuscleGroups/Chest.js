5110c6035b82ab59087b1849fe2ea503c4d9aac5
Dmitrii Prokofev <dmitriiprokofev@MacBook-Pro-Dmitrii.local>
2019-06-03 17:46:17 -0400

    added new exersizes to js file, images now fit in the box

--- client/src/pages/MuscleGroups/Chest.js
+++ client/src/pages/MuscleGroups/Chest.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
+import exersizes from "../../components/exersizes.js";
+import ExersizeCard from "../../components/ExersizeCard"
+import "../MuscleGroups/style.css"
 
-function Chest() {
-  return (
-      <div>
-        <p> Chest Workouts</p>
+
+class Chest extends Component {
+
+  state = {
+    exersizes
+  };
+  render() {
+    return (
+      <div className="exersizes">
+        {/* Gettting particular muscle group using filter method and then rendering needeed info to the page */}
+        {this.state.exersizes.filter(exersize =>
+          exersize.musclegroup === "chest").map(exersize => (
+            <ExersizeCard>
+              <p>{exersize.exersizename}</p>
+              <p>instructions</p>
+              {exersize.instructions}
+              <div className="gifs">
+                <img style={{height: 200}} src={exersize.img} alt={exersize.exersizename}  />
+              </div>
+            </ExersizeCard>
+          )
+          )}
 
       </div>
+    )
+  }
 
-  );
 };
 
 export default Chest;
--- client/src/pages/MuscleGroups/Shoulders.js
+++ client/src/pages/MuscleGroups/Shoulders.js
@@ -11,18 +11,21 @@ class Shoulders extends Component {
   };
   render() {
     return (
-      <div >
+      <div className="exersizes">
         {/* Gettting particular muscle group using filter method and then rendering needeed info to the page */}
         {this.state.exersizes.filter(exersize =>
-        exersize.musclegroup==="shoulders").map(exersize => (
-          <ExersizeCard>
-          <p>{exersize.exersizename}</p>
-          <p>instructions</p>
-          {exersize.instructions}
-          <img src={exersize.img} alt={exersize.exersizename} />
-          </ExersizeCard>
-        )
-        )}
+          exersize.musclegroup === "shoulders").map(exersize => (
+            <ExersizeCard>
+              <p>{exersize.exersizename}</p>
+              <p>instructions</p>
+              {exersize.instructions}
+              <div className="gifs">
+                <img style={{height: 200}} src={exersize.img} alt={exersize.exersizename}  />
+              </div>
+            </ExersizeCard>
+          )
+          )}
+
       </div>
     )
   }
